f916e60ebacbdd5c63575dca22196888de25fb2c
Patrick Melo <88412880+pamedsi@users.noreply.github.com>
2022-01-05 13:41:55 -0300

    first commit

---
+++ api.js
@@ -0,0 +1,37 @@
+import { even } from './numbers/even.js'
+import { odd } from './numbers/odd.js'
+import { prime } from './numbers/prime.js'
+import { multipleOfFive } from './numbers/multipleOfFive.js'
+import { fibonacci } from './numbers/fibonacci.js'
+import { perfect } from './numbers/perfect.js'
+import { intSquareRoot } from './numbers/intSquareRoot.js'
+import { triangular } from './numbers/triangular.js'
+
+export const numbers = [
+  'Numbers',
+  {
+    Types: [
+      'even',
+      'odd',
+      'prime_numbers',
+      'multipleOfFive',
+      'fibonacci',
+      'perfect',
+      'squareRoot',
+      'triangular'
+    ]
+  },
+  {
+    even: even(),
+    odd: odd(),
+    prime: prime(),
+    multipleOfFive: multipleOfFive(),
+    fibonacci: fibonacci(),
+    perfect: perfect(),
+    squareRoot: intSquareRoot(),
+    triangular: triangular()
+  }
+]
+
+const json = JSON.stringify(numbers)
+await Deno.writeTextFile('numbers.json', json)
